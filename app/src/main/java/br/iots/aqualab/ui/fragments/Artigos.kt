da2769d6189669ab803bcd621bcc588af73c131f
MaiconDequicy <maiconcosta@icomp.ufam.ufam.edu.br>
2025-09-02 17:29:18 -0400

    alteracoes e consertos no fragments

--- app/src/main/java/br/iots/aqualab/ui/fragments/Artigos.kt
+++ app/src/main/java/br/iots/aqualab/ui/fragments/Artigos.kt
@@ -10,9 +10,11 @@ import br.iots.aqualab.R
 
 class Artigos : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_artigos, container, false)
     }
 
 }
--- app/src/main/java/br/iots/aqualab/ui/fragments/Perfil.kt
+++ app/src/main/java/br/iots/aqualab/ui/fragments/Perfil.kt
@@ -10,9 +10,10 @@ import br.iots.aqualab.R
 
 class Perfil : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_perfil, container, false)
     }
-
 }
